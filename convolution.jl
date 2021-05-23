83a47fed439119758fde2b2517a7790f28a54334
Kyle Acheson <kyleacheson@bt-essentials-wi-fi-extender-928.home>
2021-05-23 11:15:30 +0100

    update kernel

--- convolution.jl
+++ convolution.jl
@@ -17,31 +17,86 @@ qAng = read(inputfile, "qAng")
 tvec = read(inputfile, "tt")
 
 
-struct Gaussian
-    Fx:: AbstractVector{Float64}
+mutable struct Gaussian
+    Fx:: Array{Float64}
     sigma:: Float64
     height:: Float64
     fwhm:: Float64
-    function Gaussian(fwhm:: Float64, x:: StepRangeLen{Float64}, x0:: Float64)
+    tconv:: StepRangeLen{Float64}
+    ntc:: Int64
+    duration:: Float64
+    dt:: Float64
+    tmin :: Float64
+    tmax:: Float64
+    nt:: Int64
+    function Gaussian(fwhm:: Float64, x:: StepRangeLen{Float64}, x0:: Float64, tmin:: Float64, tmax::Float64, nt:: Int64)
+        duration = fwhm*3
         sigma = fwhm/2sqrt(2log(2))
         height = 1/sigma*sqrt(2pi)
         Fx = height .* exp.( (-1 .* (x .- x0).^2)./ (2*sigma^2))
-        new(Fx, sigma, height, fwhm)
+        tconv = x; ntc = length(x)
+        dt = tconv[2]-tconv[1]
+        nt = nt
+        new(Fx, sigma, height, fwhm, tconv, ntc, duration, dt, tmin, tmax, nt)
+    end
+    function Gaussian(G:: Gaussian, x0:: Float64)
+        Fx = G.height .* exp.( (-1 .* (G.tconv .- x0).^2)./ (2*G.sigma^2))
+        new(Fx)
     end
 end
 
-struct Lorentzian 
-    Fx:: AbstractVector{Float64}
+
+mutable struct Lorentzian 
+    Fx:: Array{Float64}
     gamma:: Float64
     fwhm:: Float64
-    function Lorentzian(fwhm:: Float64, x:: StepRangeLen{Float64}, x0:: Float64)
+    tconv:: StepRangeLen{Float64}
+    ntc:: Int64
+    duration:: Float64
+    dt:: Float64
+    tmin :: Float64
+    tmax:: Float64
+    nt:: Int64
+    function Lorentzian(fwhm:: Float64, x:: StepRangeLen{Float64}, x0:: Float64, tmin::Float64, tmax:: Float64, nt:: Int64)
         gamma = fwhm/2
+        duration = fwhm*3
         Fx = 1 ./ ((pi * gamma) .* (1 .+ ((x .- x0)./ gamma).^2 ))
-        new(Fx, gamma, fwhm)
+        tconv = x; ntc = length(x)
+        dt = tconv[2]-tconv[1]
+        nt = nt
+        new(Fx, gamma, fwhm, tconv, ntc, duration, dt, tmin, tmax, nt)
+    end
+    function Lorentzian(L:: Lorentzian, x0:: Float64)
+        Fx = 1 ./ ((pi * L.gamma) .* (1 .+ ((L.tconv .- x0)./ L.gamma).^2 ))
+        new(Fx)
     end
 end
 
 
+function generate_kernel(fwhm:: Float64, time:: StepRangeLen{Float64}, Flag:: Int)
+
+    dt:: Float64 = sum(diff(time, dims=1))/(length(time)-1)
+    nt:: Int64 = length(time)
+    duration:: Float64 = fwhm*3 # must pad three time FHWM to deal with edges
+
+    if dt != diff(time, dims=1)[1] ; error("Non-linear time range."); end
+
+    tmin:: Float64 = minimum(time); tmax:: Float64 = maximum(time)
+    tconv_min:: Float64 = tmin - duration ; tconv_max:: Float64 = tmax + duration
+    tconv = tconv_min:dt:tconv_max # extended convoluted time vector
+
+    if Flag == 0
+        K = Gaussian(fwhm, tconv, tconv[1], tmin, tmax, nt)
+    elseif Flag == 1
+        K = Lorentzian(fwhm, tconv, tconv[1], tmin, tmax, nt)
+    else
+        error("Only Gaussian and Lorentzian functions currently available.")
+    end
+
+    return K
+
+    end
+    
 """
 # convole
 `convolve(S:: AbstractArray, fwhm:: Real, time:: AbstractArray, type:: Int)`:
@@ -63,53 +118,53 @@ Input signal will be padded by 3*FWHM before t=0, setting t<0 to the signal at t
 In addition the signal is extended by the same amount after t=end, setting t>end
 to the signal at t=end.
 """
-function convolve(S:: Array{Float64}, fwhm:: Float64, time:: Array{Float64}, type:: Int)
+
+function convolve(S:: Array{Float64}, K:: T) where {T<:Union{Gaussian, Lorentzian}}
 
     nr:: Int64, nc:: Int64 = size(S)
-    nt:: Int64 = length(time)
-    dt:: Float64 = sum(diff(time, dims=2))/(length(time)-1)
-    duration:: Float64 = fwhm*3 # must pad three time FHWM to deal with edges
 
-    if dt != diff(time, dims=2)[1] ; error("Non-linear time range."); end
-    if nr != nt && nc != nt; error("Inconsistent dimensions."); end
-    if nc != nt; S = transpose(S); nr, nc = nc, nr; end # column always time axis
+    if nr != K.nt && nc != K.nt; error("Inconsistent dimensions."); end
+    if nc != K.nt; S = transpose(S); nr, nc = nc, nr; end # column always time axis
 
-    tmin:: Float64 = minimum(time); tmax:: Float64 = maximum(time)
-    tconv_min:: Float64 = tmin - duration ; tconv_max:: Float64 = tmax + duration
-    tconv = tconv_min:dt:tconv_max # extended convoluted time vector
-    ntc:: Int64 = length(tconv) 
-    padend:: Int64 = (duration/dt) # index at which 0 padding ends and signal starts
-    padstart:: Int64 = (padend + tmax / dt) # index where signal ends and padding starts
+    padend:: Int64 = (K.duration/K.dt) # index at which 0 padding ends and signal starts
+    padstart:: Int64 = (padend + K.tmax / K.dt) # index where signal ends and padding starts
 
-    type == 0 ? Kv = Gaussian(fwhm, tconv, tconv[1]) : Kv = Lorentzian(fwhm, tconv, tconv[1])
-    println("Convolving signal with a $(typeof(Kv)) function with FWHM: $(Kv.fwhm) fs.")
+    println("Convolving signal with a $(typeof(K)) function with FWHM: $(K.fwhm) fs.")
 
-    K:: Array{Float64} = Circulant(Kv.Fx)
-    sC = zeros(Float64, nr, ntc)
+    sC = zeros(Float64, nr, K.ntc)
     sC[1:nr, 1:padend] .= S[1:nr, 1] # extended and pad signal
     sC[1:nr, padend:padstart] .= S
     sC[1:nr, padstart:end] .= S[1:nr, end]
 
-    println("""
-            Signal padded by 3*FWHM ( $(duration) fs ) forwards and backwards.
-            Original time length: $(nt), Extended time: $(ntc), Diff: $(ntc-nt) steps.
-            Padded signal size: $(size(sC)).
-            Kernel size: $(size(K[1, :])).
-            """)
-    conv = zeros(Float64, nr, ntc)
-    for t in 1:ntc
+    #println("""
+    #        Signal padded by 3*FWHM ( $(duration) fs ) forwards and backwards.
+    #        Original time length: $(nt), Extended time: $(ntc), Diff: $(ntc-nt) steps.
+    #        Padded signal size: $(size(sC)).
+    #        Kernel size: $(size(K[1, :])).
+    #        """)
+    conv = zeros(Float64, nr, K.ntc)
+    #a = 1.0
+    #dt = 0.5
+    
+    for t in 1:K.ntc
+        Gaussian(K, K.tconv[t])
+        println(t)
         for q in 1:nr
-            #conv[q, t] = sum(sC[q, 1:ntc] .* K[t, :])*dt
-            conv[q, t] = 0.0
-            for j in 1:ntc
-                conv[q, t] += sC[q, j] * K[t, j] * dt
-            end
+            #conv[q, t] = sum(collect(sC[q, 1:K.ntc]) .* collect(K.Fx[t, :]))*K.dt
+            conv[q, t] = sum(sC[q, 1:K.ntc] .* K.Fx) * K.dt
+            #for j in 1:K.ntc
+                #a += 1.0 * K.dt
+                #sC[q, t] += sC[q, j] * K.Fx[t, j]
+                #@time conv[q, t] += sC[q, j] * K.dt
+            #end
         end
     end
 
     #conv = mapslices(x -> K*x, sC; dims=2)
-    return conv, tconv
+    return conv 
 end
 
 fwhm = 100.0
-conv, tconv = convolve(signal, fwhm, tvec, 0)
+tvec = 0:0.5:1000
+kernel = generate_kernel(fwhm, tvec, 0)
+#conv, tconv = convolve(signal, kernel) 
